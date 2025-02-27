928a5877abc154dff1f222af418807c0c634d6da
Phharoah <eng.ahmed7gazy@gmail.com>
2025-02-27 13:29:38 +0200

    27/2

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: null,
     );
   }
 }
-
