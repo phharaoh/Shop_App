8a77f7d001f59c288a2712051ab7cec7c70618b2
Phharoah <eng.ahmed7gazy@gmail.com>
2025-02-27 14:48:27 +0200

    27/2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@ import 'package:flutter_test/flutter_test.dart';
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
